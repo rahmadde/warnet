9b9fb260a0f80a01a069aaefdbd9290d7e91b4a1
Rahmadde <teukurahmad120@gmail.com>
2025-07-11 01:14:25 +0700

    Membuat Edit Makanan

--- app/Http/Controllers/MakananController.php
+++ app/Http/Controllers/MakananController.php
@@ -8,6 +8,9 @@ use App\Models\Makanan;
 //return type View
 use Illuminate\View\View;
 
+//return type redirectResponse
+use Illuminate\Http\RedirectResponse;
+
 use Illuminate\Http\Request;
 
 class MakananController extends Controller
@@ -24,5 +27,37 @@ class MakananController extends Controller
 
         //render view with posts
         return view('makanans.index', compact('makanans'));
+    }
+
+    /**
+     * create
+     *
+     * @return View
+     */
+    public function create(): View
+    {
+        return view('makanans.create');
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+
+        //create post
+        Makanan::create([
+            'id_pemesanan'     => $request->id_pemesanan,
+            'nm_makanan'   => $request->nm_makanan,
+            'id_member'   => $request->id_member,
+            'jumlah'   => $request->jumlah,
+            'tgl_pemesanan'   => $request->tgl_pemesanan,
+        ]);
+
+        //redirect to index
+        return redirect()->route('makanans.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 }
